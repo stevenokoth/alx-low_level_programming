c238244cccaef9e1114e35079a384ea47909c1bc
stevenokoth <stevenocholao@gmail.com>
2022-12-08 14:46:51 -0800

    somefiles of C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * returns zero when exited in correct way, otherswise, a non-zero is the result
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
