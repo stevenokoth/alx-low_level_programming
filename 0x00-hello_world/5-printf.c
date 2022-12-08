869a48338dd3f55602311e3fcdc446b99a7a7c56
stevenokoth <stevenocholao@gmail.com>
2022-12-08 15:37:43 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,12 @@
-later
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	pintf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
