f03303f70e5043722516239227c8b1da56121bfc
stevenokoth <stevenocholao@gmail.com>
2022-12-08 23:54:04 -0800

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art,
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
