dd510841e5d074a466858475559dd449b5f43467
stevenokoth <stevenocholao@gmail.com>
2022-12-14 03:16:05 -0800

    netestedloops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*print_alphabet - prints the alphabet, in lowercas, followed by a new line.
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
+	_putchar('\n');
+}
