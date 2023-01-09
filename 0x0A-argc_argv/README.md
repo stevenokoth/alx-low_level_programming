5baf4f3561c493ecdb4fe33d913697b6c535e8ba
stevenokoth <stevenocholao@gmail.com>
2023-01-09 12:34:36 -0800

    argc and argv

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+#__0x0A-argc_argv
+This is all about argc mad argv commands
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define NAIN_H
+
+#include<stdio.h>
+
+int _putchar(char c);
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[]);
+int _atoi(char *s);
+
+#endif /* MAIN_H */
